7d16fb3ca2380fb757fb20dccb614dee39f049f3
YumNumm <onory0917@gmail.com>
2023-04-05 18:45:49 +0900

    fix: ファイル名を修正

--- lib/features/weather/view/weather_view.dart
+++ lib/features/weather/view/weather_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/features/weather/components/weather_item_widget.dart';
+import 'package:flutter_training/features/weather/components/weather_temperature_widget.dart';
 
 class WeatherView extends StatefulWidget {
   const WeatherView({super.key});
