ab6078c3b7b2b95d7e630be7c79f615af0369dcb
YumNumm <onory0917@gmail.com>
2023-04-06 14:06:33 +0900

    style: weatherCondition変数名を修正

--- lib/features/weather/components/weather_icon_widget.dart
+++ lib/features/weather/components/weather_icon_widget.dart
@@ -3,16 +3,16 @@ import 'package:flutter_svg/svg.dart';
 import 'package:flutter_training/features/weather/model/weather_condition.dart';
 
 class WeatherIconWidget extends StatelessWidget {
-  const WeatherIconWidget({required this.weather, super.key});
+  const WeatherIconWidget({required this.weatherCondition, super.key});
 
-  final WeatherCondition? weather;
+  final WeatherCondition? weatherCondition;
 
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 1,
-      child: weather != null
-          ? SvgPicture.asset(weather!.svgImagePath)
+      child: weatherCondition != null
+          ? SvgPicture.asset(weatherCondition!.svgImagePath)
           : const Placeholder(),
     );
   }
