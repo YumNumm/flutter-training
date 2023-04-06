fa9a49769b49846377247a706d5980eb39244278
YumNumm <onory0917@gmail.com>
2023-04-06 17:16:08 +0900

    add: 画面遷移の追加

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,7 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_training/constants/route.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    unawaited(
+      () async {
+        await Future<void>.delayed(
+          const Duration(milliseconds: 500),
+        );
+        if (context.mounted) {
+          const WeatherRoute().push(context);
+        }
+      }(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
