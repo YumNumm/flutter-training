4f874bcbeb716629dba4939e3426492e0389f1d1
YumNumm <onory0917@gmail.com>
2023-04-07 09:51:47 +0900

    fix: SplashScreenからの遷移改善

--- lib/features/splash/screen/splash_screen.dart
+++ lib/features/splash/screen/splash_screen.dart
@@ -11,18 +11,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<SplashScreen> {
+  /// WeatherScreenに遷移する
+  /// WeatherScreenからpopされたら、指定時間経過後 再度WeatherScreenに遷移する
+  Future<void> delayedTransitToWeatherScreenWithLoop() async {
+    await Future<void>.delayed(
+      const Duration(milliseconds: 500),
+    );
+    if (context.mounted) {
+      // WeatherScreenに遷移する
+      await router.push(const WeatherRoute().location);
+      // popしたので再度遷移する
+      await delayedTransitToWeatherScreenWithLoop();
+    }
+  }
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Timer(
+        const Duration(milliseconds: 500),
+        delayedTransitToWeatherScreenWithLoop,
+      );
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.endOfFrame.then((_) {
-      // 500ms後にSplashRouteに遷移する
-      Timer(const Duration(milliseconds: 500), () {
-        if (context.mounted &&
-            router.location != const WeatherRoute().location) {
-          const WeatherRoute().push(context);
-        }
-      });
-    });
     return const SizedBox.expand(
       child: ColoredBox(
         color: Colors.green,
