084e8071a8890ba2854b7bbdf057375d129908b8
YumNumm <onory0917@gmail.com>
2023-04-12 10:09:58 +0900

    fix: 不要なenum `fromString`関数を消去

--- lib/features/weather/model/weather_condition.dart
+++ lib/features/weather/model/weather_condition.dart
@@ -1,13 +1,5 @@
-import 'package:collection/collection.dart';
-
 enum WeatherCondition {
   sunny,
   cloudy,
   rainy;
-
-  static WeatherCondition? fromString(String input) {
-    return WeatherCondition.values.firstWhereOrNull(
-      (e) => e.name == input,
-    );
-  }
 }
