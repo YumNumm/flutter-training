8afadac0d7c314b70ac9f80146c52a30152b3f0a
YumNumm <onory0917@gmail.com>
2023-04-17 08:29:11 +0900

    fix: riverpod_generatorで統一

--- lib/common/provider/router_provider.dart
+++ lib/common/provider/router_provider.dart
@@ -2,9 +2,12 @@ import 'package:flutter_training/constants/route.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final routerProvider = Provider<GoRouter>(
-  (ref) => GoRouter(
+part 'router_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+GoRouter router(RouterRef ref) {
+  return GoRouter(
     routes: $appRoutes,
     initialLocation: const SplashRoute().location,
-  ),
-);
+  );
+}
---
+++ lib/common/provider/router_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'router_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$routerHash() => r'a5c9434500d60eeae0122380a9e114211a6a8545';
+
+/// See also [router].
+@ProviderFor(router)
+final routerProvider = Provider<GoRouter>.internal(
+  router,
+  name: r'routerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RouterRef = ProviderRef<GoRouter>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
