0b32011df99432acdc737c4984f69c154f4fb414
YumNumm <onory0917@gmail.com>
2023-04-20 12:08:57 +0900

    add: useCaseを追加

---
+++ lib/common/use_case/fake_use_case.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_training/common/use_case/use_case.dart';
+
+/// UseCaseのテスト用のダミークラス
+/// test内で利用する
+/// ```dart
+/// class FakeSampleUseCase extends FakeUseCase<int, String>
+///    implements SampleUseCase {}
+///
+/// void test() {
+///   late final FakeSampleUseCase useCase;
+///   setUp(() => useCase = FakeSampleUseCase());
+///   test('test', () {
+///     // usecCaseが返すものを指定
+///     useCase.result('result: 1');
+///     //useCase.resultCallback = (param) => 'result: $param';
+///     expect(useCase.call(1), 'result: 1');
+///   });
+/// }
+///
+/// ```
+class FakeUseCase<Param, Result> extends UseCase<Param, Result> {
+  late final Result Function(Param)? _callback;
+
+  // ignore: avoid_setters_without_getters
+  set resultCallback(Result Function(Param param) callback) {
+    _callback = callback;
+  }
+
+  void result(Result v) {
+    _callback = (_) => v;
+  }
+
+  @override
+  Result call(Param param) => _callback!(param);
+}
