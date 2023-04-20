cc173a0f43f9e55e714ee26cd763843400f0eb83
YumNumm <onory0917@gmail.com>
2023-04-20 19:10:35 +0900

    add: Result型にvalueOrNull/errorOrNullを追加

--- lib/common/model/result.dart
+++ lib/common/model/result.dart
@@ -4,6 +4,17 @@ part 'result.freezed.dart';
 
 @freezed
 class Result<T, R> with _$Result<T, R> {
+  const Result._();
   const factory Result.success(T value) = Success<T, R>;
   const factory Result.failure(R error) = Failure<T, R>;
+
+  T? get valueOrNull => when(
+        success: (value) => value,
+        failure: (_) => null,
+      );
+
+  R? get errorOrNull => when(
+        success: (_) => null,
+        failure: (error) => error,
+      );
 }
