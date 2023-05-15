edf1a7509bd3a3417e0619ee1d6bdcd6c351af54
YumNumm <onory0917@gmail.com>
2023-05-15 14:54:06 +0900

    add: FixtureFactoryの作成

---
+++ test/features/weather/model/fetch_weather_request_fixture.dart
@@ -0,0 +1,31 @@
+import 'package:data_fixture_dart/definitions/fixture_definition.dart';
+import 'package:data_fixture_dart/definitions/json_fixture_definition.dart';
+import 'package:data_fixture_dart/factories/json_fixture_factory.dart';
+import 'package:flutter_training/features/weather/model/fetch_weather_request.dart';
+
+extension FetchWeatherRequestFixture on FetchWeatherRequest {
+  // ignore: library_private_types_in_public_api
+  static _FetchWeatherRequestFixtureFactory get fixture =>
+      _FetchWeatherRequestFixtureFactory();
+}
+
+class _FetchWeatherRequestFixtureFactory
+    extends JsonFixtureFactory<FetchWeatherRequest> {
+  @override
+  FixtureDefinition<FetchWeatherRequest> definition() => define(
+        (faker) {
+          return FetchWeatherRequest(
+            area: faker.lorem.word(),
+            date: faker.date.dateTime(),
+          );
+        },
+      );
+
+  @override
+  JsonFixtureDefinition<FetchWeatherRequest> jsonDefinition() => defineJson(
+        (request) => <String, dynamic>{
+          'area': request.area,
+          'date': request.date.toIso8601String(),
+        },
+      );
+}
---
+++ test/features/weather/model/fetch_weather_response_fixture.dart
@@ -0,0 +1,37 @@
+import 'package:data_fixture_dart/definitions/fixture_definition.dart';
+import 'package:data_fixture_dart/definitions/json_fixture_definition.dart';
+import 'package:data_fixture_dart/factories/json_fixture_factory.dart';
+import 'package:flutter_training/features/weather/model/fetch_weather_response.dart';
+import 'package:flutter_training/features/weather/model/weather_condition.dart';
+
+extension FetchWeatherResponseFixture on FetchWeatherResponse {
+  // ignore: library_private_types_in_public_api
+  static _FetchWeatherResponseFixtureFactory get fixture =>
+      _FetchWeatherResponseFixtureFactory();
+}
+
+class _FetchWeatherResponseFixtureFactory
+    extends JsonFixtureFactory<FetchWeatherResponse> {
+  @override
+  FixtureDefinition<FetchWeatherResponse> definition() => define(
+        (faker) {
+          return FetchWeatherResponse(
+            date: faker.date.dateTime(),
+            maxTemperature: faker.randomGenerator.integer(min: 20, 50),
+            minTemperature: faker.randomGenerator.integer(min: 10, 20),
+            weatherCondition:
+                faker.randomGenerator.element(WeatherCondition.values),
+          );
+        },
+      );
+
+  @override
+  JsonFixtureDefinition<FetchWeatherResponse> jsonDefinition() => defineJson(
+        (request) => <String, dynamic>{
+          'date': request.date.toIso8601String(),
+          'max_temperature': request.maxTemperature,
+          'min_temperature': request.minTemperature,
+          'weather_condition': request.weatherCondition.name,
+        },
+      );
+}
