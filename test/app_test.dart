6466e4cf5fbaf738571939b5d72e04383b03d3e1
Ryotaro Onoue <73390859+YumNumm@users.noreply.github.com>
2023-05-23 16:54:09 +0900

    Session 10: Widget Test (#40)
    * add: weather component test
    
    * fix: displaySize util
    
    * Revert "fix: displaySize util"
    
    This reverts commit 42acef2e8c4717b3c832d688a5db0593ebb47838.
    
    * add: app/weatherScreenのテスト
    
    * revert: SvgPictureのkey追加
    
    * fix: testWidgetsの引数命名を統一
    
    * revert: weather_viewmodel.g.dart
    
    * style: appTestのdescriptionを修正
    
    * style: 不要なgroupの消去
    
    * remove: WeatherIconWidgetTestの一部を消去
    - WeatherScreenTestで同様の実装がしてあります。
    
    * fix: svgImageのテスト修正
    
    * fix: removed unused import

---
+++ test/app_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/app.dart';
+import 'package:flutter_training/features/splash/screen/splash_screen.dart';
+import 'package:flutter_training/features/weather/screen/weather_screen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'utils/display_size.dart';
+
+void main() {
+  setUp(setDisplaySize);
+  tearDown(clearDisplaySize);
+
+  testWidgets('500ms後に自動的にSplashScreenからWeatherScreenに遷移すること', (tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: App(),
+      ),
+    );
+    expect(find.byType(SplashScreen), findsOneWidget);
+    // 500ms待つ
+    await tester.pump(const Duration(milliseconds: 500));
+    await tester.pumpAndSettle();
+    expect(find.byType(SplashScreen), findsNothing);
+    expect(find.byType(WeatherScreen), findsOneWidget);
+  });
+}
---
+++ test/features/weather/components/weather_icon_widget_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/features/weather/components/weather_icon_widget.dart';
+
+void main() {
+  testWidgets('weatherConditionがnullの場合 Placeholderを表示する', (tester) async {
+    await tester.pumpWidget(
+      const WeatherIconWidget(weatherCondition: null),
+    );
+    expect(find.byType(Placeholder), findsOneWidget);
+    expect(find.byType(SvgPicture), findsNothing);
+  });
+}
