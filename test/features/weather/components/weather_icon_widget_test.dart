1c3de5be556eec72ea4af82c566e794fd7befe08
YumNumm <onory0917@gmail.com>
2023-05-23 16:47:00 +0900

    fix: removed unused import

--- test/features/weather/components/weather_icon_widget_test.dart
+++ test/features/weather/components/weather_icon_widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_training/features/weather/components/weather_icon_widget.dart';
-import 'package:flutter_training/features/weather/model/weather_condition.dart';
 
 void main() {
   testWidgets('weatherConditionがnullの場合 Placeholderを表示する', (tester) async {
