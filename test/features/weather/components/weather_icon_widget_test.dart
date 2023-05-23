0f36b6cc536854dfc43a19663436fd20cddb6554
YumNumm <onory0917@gmail.com>
2023-05-23 15:15:02 +0900

    style: 不要なgroupの消去

--- test/features/weather/components/weather_icon_widget_test.dart
+++ test/features/weather/components/weather_icon_widget_test.dart
@@ -5,35 +5,29 @@ import 'package:flutter_training/features/weather/components/weather_icon_widget
 import 'package:flutter_training/features/weather/model/weather_condition.dart';
 
 void main() {
-  group(
-    'WeatherIconWidget',
-    () {
-      testWidgets('weatherConditionがnullの場合 Placeholderを表示する', (tester) async {
-        await tester.pumpWidget(
-          const WeatherIconWidget(weatherCondition: null),
-        );
-        expect(find.byType(Placeholder), findsOneWidget);
-        expect(find.byType(SvgPicture), findsNothing);
-      });
-      testWidgets('weatherConditionが指定された場合 正しいSvgPictureを表示する',
-          (tester) async {
-        for (final weatherCondition in WeatherCondition.values) {
-          await tester.pumpWidget(
-            WeatherIconWidget(
-              weatherCondition: weatherCondition,
-            ),
-          );
-          expect(find.byType(Placeholder), findsNothing);
-          expect(find.byType(SvgPicture), findsOneWidget);
+  testWidgets('weatherConditionがnullの場合 Placeholderを表示する', (tester) async {
+    await tester.pumpWidget(
+      const WeatherIconWidget(weatherCondition: null),
+    );
+    expect(find.byType(Placeholder), findsOneWidget);
+    expect(find.byType(SvgPicture), findsNothing);
+  });
+  testWidgets('weatherConditionが指定された場合 正しいSvgPictureを表示する', (tester) async {
+    for (final weatherCondition in WeatherCondition.values) {
+      await tester.pumpWidget(
+        WeatherIconWidget(
+          weatherCondition: weatherCondition,
+        ),
+      );
+      expect(find.byType(Placeholder), findsNothing);
+      expect(find.byType(SvgPicture), findsOneWidget);
 
-          // Assert
-          expect(find.byType(WeatherIconWidget), findsOneWidget);
-          final weatherIconWidget = tester.widget<WeatherIconWidget>(
-            find.byType(WeatherIconWidget),
-          );
-          expect(weatherIconWidget.weatherCondition, weatherCondition);
-        }
-      });
-    },
-  );
+      // Assert
+      expect(find.byType(WeatherIconWidget), findsOneWidget);
+      final weatherIconWidget = tester.widget<WeatherIconWidget>(
+        find.byType(WeatherIconWidget),
+      );
+      expect(weatherIconWidget.weatherCondition, weatherCondition);
+    }
+  });
 }
