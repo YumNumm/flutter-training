72f5c972ca7e4034f93e957977f8463279d00162
YumNumm <onory0917@gmail.com>
2023-05-23 15:19:27 +0900

    remove: WeatherIconWidgetTestの一部を消去 - WeatherScreenTestで同様の実装がしてあります。

--- test/features/weather/components/weather_icon_widget_test.dart
+++ test/features/weather/components/weather_icon_widget_test.dart
@@ -12,22 +12,4 @@ void main() {
     expect(find.byType(Placeholder), findsOneWidget);
     expect(find.byType(SvgPicture), findsNothing);
   });
-  testWidgets('weatherConditionが指定された場合 正しいSvgPictureを表示する', (tester) async {
-    for (final weatherCondition in WeatherCondition.values) {
-      await tester.pumpWidget(
-        WeatherIconWidget(
-          weatherCondition: weatherCondition,
-        ),
-      );
-      expect(find.byType(Placeholder), findsNothing);
-      expect(find.byType(SvgPicture), findsOneWidget);
-
-      // Assert
-      expect(find.byType(WeatherIconWidget), findsOneWidget);
-      final weatherIconWidget = tester.widget<WeatherIconWidget>(
-        find.byType(WeatherIconWidget),
-      );
-      expect(weatherIconWidget.weatherCondition, weatherCondition);
-    }
-  });
 }
